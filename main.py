fe386e45c2750434bad8cf81f09b2ce0a1e5f5f3
boyentenbi <pchoy95@gmail.com>
2020-04-26 21:38:24 +0800

    Add food, collision handler for tadpoles and food, and keep track of scores of each body

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def add_ball(space):
     x = random.randint(120,380)
     body.position = x, 550
     shape = pymunk.Circle(body, radius, (0,0))
+    shape.collision_type = 0
     space.add(body, shape)
     return shape
 
@@ -30,7 +31,20 @@ def add_wall(line_end, line_start, space):
     return shape
 
 
+def add_food(pos, space):
+    body = pymunk.Body(body_type=pymunk.Body.STATIC)
+    body.position = pos
+    radius=14
+    shape = pymunk.Circle(body, radius)
+    shape.collision_type = 1
+    space.add(body, shape)
+    return shape
+
+
 def main():
+
+    n_tadpoles = 10
+
     pygame.init()
     screen = pygame.display.set_mode((600, 600))
     pygame.display.set_caption("Some tadpoles swimming randomly.")
@@ -40,16 +54,41 @@ def main():
     # space.gravity = (0.0, -90.0)
     space.damping = 0.1
 
-
+    # Add walls.
     for line_start, line_end in [((0, 0), (0, 600)),
                                  ((0, 600), (600, 600)),
                                  ((600, 600), (600, 0)),
                                  ((600, 0), (0, 0))]:
         add_wall(line_end, line_start, space)
 
+    # Add food.
+    food_objects = []
+    food_positions = itertools.product(range(100, 600, 100), range(100, 600, 100))
+    for pos in food_positions:
+        food_object = add_food(pos, space)
+        food_objects.append(food_object)
 
     taddies = [add_ball(space) for _ in range(10)]
     policies = [renderer.RandomPolicy() for taddy in taddies]
+
+    # Initialize scores
+    scores = {tadpole: 0 for tadpole in taddies}
+
+    # Add food-tadpole collision handler
+    food_tadpole_handler = space.add_collision_handler(0, 1)
+
+    def food_tadpole_handler_begin(arbiter, space, data):
+        tadpole, food = arbiter.shapes
+        scores[tadpole] += 1
+        space.remove(food)
+        food_objects.remove(food)
+        print("Food eaten!")
+        return False
+
+    food_tadpole_handler.begin = food_tadpole_handler_begin
+    # food_tadpole_handler.pre_solve = lambda arbiter, space, data: True
+    # food_tadpole_handler.post_solve = lambda arbiter, space, data: print("post_solve")
+    # food_tadpole_handler.separate = lambda arbiter, space, data: None
 
     draw_options = pymunk.pygame_util.DrawOptions(screen)
 
@@ -58,7 +97,7 @@ def main():
             force, rot = tuple(policy())
             force_vector = 300 * 1 * taddy.body.rotation_vector
             taddy.body.apply_force_at_local_point(force_vector, (0, 0))
-            rot_force = 1000* rot * taddy.body.rotation_vector.rotated(np.pi/2) # orthogonal to the rot vector, magnitude rot
+            rot_force = 1000 * rot * taddy.body.rotation_vector.rotated(np.pi/2) # orthogonal to the rot vector, magnitude rot
             rot_force_position = taddy.body.rotation_vector # unit distance in the direction of the rot vector
             taddy.body.apply_force_at_local_point(rot_force, rot_force_position)
             taddy.body.apply_force_at_local_point(-rot_force, -rot_force_position)
@@ -69,6 +108,10 @@ def main():
                 sys.exit(0)
             elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 sys.exit(0)
+
+        if not food_objects:
+            print(scores)
+            sys.exit()
 
         screen.fill((255,255,255))
 
