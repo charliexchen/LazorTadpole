61bfc464799e7a1176a97331134dad8b68b565d1
boyentenbi <pchoy95@gmail.com>
2020-05-03 13:16:41 +0800

    Add HEADLESS global and refactor evaluation into evaluate method

--- main.py
+++ main.py
@@ -40,88 +40,96 @@ def add_food(pos, space):
     space.add(body, shape)
     return shape
 
+HEADLESS = False
+
 
 def main():
 
     n_tadpoles = 10
+    damping = 0.01
+    screen_dims = (600, 600)
+    policies = [renderer.RandomPolicy() for _ in range(n_tadpoles)]
+    evaluate(n_tadpoles, damping, screen_dims, policies)
 
-    pygame.init()
-    screen = pygame.display.set_mode((600, 600))
-    pygame.display.set_caption("Some tadpoles swimming randomly.")
-    clock = pygame.time.Clock()
 
+def evaluate(n_tadpoles, damping, screen_dims, policies):
+    if not HEADLESS:
+        pygame.init()
+        screen = pygame.display.set_mode(screen_dims)
+        pygame.display.set_caption("Some tadpoles swimming randomly.")
+        clock = pygame.time.Clock()
+        draw_options = pymunk.pygame_util.DrawOptions(screen)
+    else:
+        screen = None
+        draw_options = None
+        clock = None
     space = pymunk.Space()
     # space.gravity = (0.0, -90.0)
-    space.damping = 0.1
-
+    space.damping = damping
     # Add walls.
+    line_endpoints = []
     for line_start, line_end in [((0, 0), (0, 600)),
                                  ((0, 600), (600, 600)),
                                  ((600, 600), (600, 0)),
                                  ((600, 0), (0, 0))]:
         add_wall(line_end, line_start, space)
-
     # Add food.
     food_objects = []
     food_positions = itertools.product(range(100, 600, 100), range(100, 600, 100))
     for pos in food_positions:
         food_object = add_food(pos, space)
         food_objects.append(food_object)
-
-    taddies = [add_ball(space) for _ in range(10)]
-    policies = [renderer.RandomPolicy() for taddy in taddies]
+    taddy_balls = [add_ball(space) for _ in range(n_tadpoles)]
+    taddy_ball_mapping = {taddy_ball: i for i, taddy_ball in enumerate(taddy_balls)}
 
     # Initialize scores
-    scores = {tadpole: 0 for tadpole in taddies}
-
+    scores = {i: 0 for i in range(n_tadpoles)}
     # Add food-tadpole collision handler
     food_tadpole_handler = space.add_collision_handler(0, 1)
 
     def food_tadpole_handler_begin(arbiter, space, data):
-        tadpole, food = arbiter.shapes
+        tadpole_ball, food = arbiter.shapes
+        tadpole = taddy_ball_mapping[tadpole_ball]
         scores[tadpole] += 1
         space.remove(food)
         food_objects.remove(food)
-        print("Food eaten!")
         return False
 
     food_tadpole_handler.begin = food_tadpole_handler_begin
     # food_tadpole_handler.pre_solve = lambda arbiter, space, data: True
     # food_tadpole_handler.post_solve = lambda arbiter, space, data: print("post_solve")
     # food_tadpole_handler.separate = lambda arbiter, space, data: None
-
-    draw_options = pymunk.pygame_util.DrawOptions(screen)
-
     while True:
-        for taddy, policy in zip(taddies, policies):
+        for taddy, policy in zip(taddy_balls, policies):
             force, rot = tuple(policy())
             force_vector = 300 * 1 * taddy.body.rotation_vector
             taddy.body.apply_force_at_local_point(force_vector, (0, 0))
-            rot_force = 1000 * rot * taddy.body.rotation_vector.rotated(np.pi/2) # orthogonal to the rot vector, magnitude rot
-            rot_force_position = taddy.body.rotation_vector # unit distance in the direction of the rot vector
+            rot_force = 1000 * rot * taddy.body.rotation_vector.rotated(
+                np.pi / 2)  # orthogonal to the rot vector, magnitude rot
+            rot_force_position = taddy.body.rotation_vector  # unit distance in the direction of the rot vector
             taddy.body.apply_force_at_local_point(rot_force, rot_force_position)
             taddy.body.apply_force_at_local_point(-rot_force, -rot_force_position)
-
-
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                sys.exit(0)
-            elif event.type == KEYDOWN and event.key == K_ESCAPE:
-                sys.exit(0)
 
         if not food_objects:
             print(scores)
             sys.exit()
 
-        screen.fill((255,255,255))
+        if not HEADLESS:
 
-        space.debug_draw(draw_options)
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    sys.exit(0)
+                elif event.type == KEYDOWN and event.key == K_ESCAPE:
+                    sys.exit(0)
 
-        space.step(1/50.0)
+            screen.fill((255, 255, 255))
 
-        pygame.display.flip()
-        clock.tick(50)
+            space.debug_draw(draw_options)
 
+            pygame.display.flip()
+            clock.tick(50)
+
+        space.step(1 / 50.0)
 
 
 if __name__ == '__main__':
